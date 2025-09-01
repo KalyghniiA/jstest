f7bf38599e76efa16af24cdca88da2c01fa8ed7b
Александр <kalyghnii@gmail.com>
2025-09-01 14:25:09 +0500

    create formula

--- 4-navigator/index.js
+++ 4-navigator/index.js
@@ -1,1 +1,8 @@
-console.log('Happy developing ✨')
+let addressLat;
+let addressLong;
+let positionLat;
+let positionLong;
+
+function calcDistance (x1, y1, x2, y2) {
+    return Math.sqrt((x2 - x1) **2 + (y2 - y1) **2);
+}
