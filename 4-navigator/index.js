720ffbdf58d0dc1a712c7a3141ff878d070fb918
Александр <kalyghnii@gmail.com>
2025-09-01 14:40:57 +0500

    create formula

--- 4-navigator/index.js
+++ 4-navigator/index.js
@@ -4,5 +4,7 @@ let positionLat;
 let positionLong;
 
 function calcDistance (x1, y1, x2, y2) {
-    return Math.sqrt((x2 - x1) **2 + (y2 - y1) **2);
+    return 2 * Math.asin(Math.sqrt((Math.sin((x2 - x1) / 2) ** 2) + Math.cos(x1) * Math.cos(x2) * (Math.sin((y2 - y2) / 2) ** 2)));
 }
+
+calcDistance(addressLat, positionLat, addressLong, positionLong);
