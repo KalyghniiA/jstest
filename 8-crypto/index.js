854e2238d1e12372fd6b0569f1bb1d570d7c2936
Александр <kalyghnii@gmail.com>
2025-09-05 12:25:40 +0500

    create

--- 8-crypto/index.js
+++ 8-crypto/index.js
@@ -1,11 +1,11 @@
-const toCrypto = (pass) => {
+const crypto = (pass) => {
     const symbols = pass.split("");
     const leftReverse = symbols.slice(0, symbols.length / 2 ).reverse();
     const rightReverse = symbols.slice(symbols.length / 2, symbols.length).reverse();
     return leftReverse.concat(rightReverse).join("");
 }
 
-const checkEqualsPassword = (pass, cryptoPass) => {
+const check = (pass, cryptoPass) => {
     return pass === toCrypto(cryptoPass);
 }
 
