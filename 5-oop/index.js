30d545006ccb13f783118ef900fe87625b6f3a13
Александр <kalyghnii@gmail.com>
2025-09-25 13:02:50 +0500

    create

--- 5-oop/index.js
+++ 5-oop/index.js
@@ -10,22 +10,28 @@ Character.prototype.speak = function () {
     console.log(`${this.language} ${this.name}`);
 }
 
-const Orc = function (name, language) {
+const Orc = function (name, language, weapon) {
     Character.call(this, "orc", name, language);
+    this.weapon = weapon;
 }
 
 Orc.prototype = Object.create(Character.prototype);
 Orc.prototype.constructor = Orc;
-Orc.prototype.hit = function () {console.log("hit")}
-Orc.prototype.spell = function () {console.log("spell")}
-Orc.prototype.createSpell = function () {
-    console.log("createSpell");
+Orc.prototype.hit = function () {console.log(`hit ${this.weapon}`)}
+
+const Elf = function (name, language, typeSpell) {
+    Character.call(this, "elf", name, language);
+    this.typeSpell = typeSpell;
+}
+
+Elf.prototype = Object.create(Character.prototype);
+Elf.prototype.constructor = Elf;
+
+Elf.prototype.spell = function () {console.log("spell")}
+Elf.prototype.createSpell = function () {
+    console.log(`createSpell type ${this.typeSpell}`);
     this.spell();
 }
 
 
-const orc = new Orc("jack", "ru");
-console.log(orc);
-orc.speak();
-orc.createSpell()
 
