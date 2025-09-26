5c3110d2a04cc49a64e2c3540742fd9a03019093
Александр <kalyghnii@gmail.com>
2025-09-26 16:19:14 +0500

    create

---
+++ 9-pokemon/index.js
@@ -0,0 +1,21 @@
+const getPokemonEffect = () => {
+    const url = " https://pokeapi.co/api/v2/pokemon/ditto";
+    const res = new XMLHttpRequest();
+    res.open("GET", url);
+    res.send();
+
+    res.addEventListener("load", () => {
+        const pokemon = JSON.parse(res.responseText);
+        const abilityUrl = pokemon.abilities[0].ability.url;
+
+        const res2 = new XMLHttpRequest();
+        res2.open("GET", abilityUrl);
+        res2.send();
+
+        res2.addEventListener("load", () => {
+            const ability = JSON.parse(res2.responseText);
+            console.log(ability.effect_entries[0].effect);
+        })
+    })
+}
+getPokemonEffect();
