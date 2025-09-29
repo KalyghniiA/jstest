e1f409514acfab22a51129e225aaf295b18412e8
Александр <kalyghnii@gmail.com>
2025-09-29 21:30:17 +0500

    create

--- 12-promise-race/index.js
+++ 12-promise-race/index.js
@@ -1,11 +1,6 @@
 const race = async (promises) => {
     try {
-        for (let promise of promises) {
-            const resolve = await promise.resolve();
-            if (resolve.status === "fulfilled") {
-                return resolve;
-            }
-        }
+        return await Promise.race(promises);
     } catch (error) {
         console.error(error);
     }
